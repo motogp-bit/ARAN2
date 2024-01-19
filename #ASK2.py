aa6d0fe19b5d1b673ebae733fe728661b81e7a7e
motogp-bit <78108539+motogp-bit@users.noreply.github.com>
2024-01-19 18:56:27 +0200

    Update #ASK2.py

--- #ASK2.py
+++ #ASK2.py
@@ -1,34 +1,28 @@
-#ASK2
 import numpy as np
-step = np.pi/24.0
+step = np.pi/20.0
 a = 0
 arr = []
 arr.append(a)
 while (a+step <(np.pi)/2.0):
     arr.append(a+step)
     a+=step
-#print(arr)
 def simpson():
-    sum = 0
-    for i in range(12):
-        if i==0 or i==11:
-            sum+=np.sin(arr[i])
-        elif i % 2 == 1:
+    sum = np.sin(0) + np.sin(np.pi/2)
+    for i in range(1,10):
+        if i % 2 == 1:
             sum+= 4*np.sin(arr[i])
         elif i % 2 == 0:
             sum+= 2*np.sin(arr[i])
-    return (np.pi/72.0) *sum
-print(simpson())
+    return (step/3) *sum
+#print(simpson())
 
 
 
 def trapezoid():
-    sum = 0
-    for i in range(12):
-        if i==0 or i==11:
-            sum+=np.sin(arr[i])
-        else:
-            sum+= 2* np.sin(arr[i])
+    sum= np.sin(0) + np.sin(np.pi/2)
+    for i in range(1,10):
+        sum+= 2*np.sin(arr[i])
+        print(sum)
     sum*= step/2
     return sum
-print(trapezoid())
+#print(trapezoid())
